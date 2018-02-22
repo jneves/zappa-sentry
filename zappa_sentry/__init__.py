fd6c0757f34d966ac1dac2b6e0c3d4ad09a23198
João Miguel Neves <joao@silvaneves.org>
2018-02-22 20:46:02 +0000

    change zappa_client to raven_client

--- zappa_sentry/__init__.py
+++ zappa_sentry/__init__.py
@@ -3,19 +3,19 @@ import os
 
 from raven import Client
 
-zappa_client = Client(os.environ['SENTRY_DSN'])
+raven_client = Client(os.environ['SENTRY_DSN'])
 
 
 def unhandled_exceptions(e, event, context):
     package_info_file = open('package_info.json', 'r')
     package_info = json.load(package_info_file)
     package_info_file.close()
-    zappa_client.setTagsContext(package_info)
+    raven_client.setTagsContext(package_info)
 
-    zappa_client.setExtraContext({
+    raven_client.setExtraContext({
         'event': event,
         'context': context
     })
 
-    zappa_client.captureException()
+    raven_client.captureException()
     return True
