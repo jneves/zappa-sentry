3cd4ce97a25b4b4b6ecaf76f628bddecfcd19767
João Miguel Neves <joao@silvaneves.org>
2018-02-22 21:45:34 +0000

    new try

--- zappa_sentry/__init__.py
+++ zappa_sentry/__init__.py
@@ -11,12 +11,13 @@ def unhandled_exceptions(e, event, context):
     package_info_file = open('package_info.json', 'r')
     package_info = json.load(package_info_file)
     package_info_file.close()
-    raven_client.setTagsContext(package_info)
 
-    raven_client.setExtraContext({
-        'event': event,
-        'context': context
-    })
+    raven_client.context.merge({'tags': package_info})
+
+    #raven_client.setExtraContext({
+    #    'event': event,
+    #    'context': context
+    #})
 
     raven_client.captureException(e)
     return True
