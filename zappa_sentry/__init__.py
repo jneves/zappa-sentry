6d5083ea2f209730aae83617bc888647e1f33155
João Miguel Neves <joao@silvaneves.org>
2018-02-25 08:17:32 +0000

    revert changes, issue was in zappa

--- zappa_sentry/__init__.py
+++ zappa_sentry/__init__.py
@@ -7,17 +7,16 @@ raven_client = Client(os.environ['SENTRY_DSN'])
 
 
 def unhandled_exceptions(e, event, context):
-    print('unhandled exceptions')
     package_info_file = open('package_info.json', 'r')
     package_info = json.load(package_info_file)
     package_info_file.close()
 
     raven_client.context.merge({'tags': package_info})
 
-    #raven_client.setExtraContext({
-    #    'event': event,
-    #    'context': context
-    #})
+    raven_client.setExtraContext({
+        'event': event,
+        'context': context
+    })
 
     raven_client.captureException(e)
     return True
