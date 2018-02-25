da6651b8073dcc9986294e70bbb74e6bf7e9d258
João Miguel Neves <joao@silvaneves.org>
2018-02-25 11:11:02 +0000

    re-enable extra tags

--- zappa_sentry/__init__.py
+++ zappa_sentry/__init__.py
@@ -7,11 +7,16 @@ raven_client = Client(os.environ['SENTRY_DSN'])
 
 
 def unhandled_exceptions(e, event, context):
-    package_info_file = open('package_info.json', 'r')
-    package_info = json.load(package_info_file)
-    package_info_file.close()
+    try:
+        package_info_file = open('package_info.json', 'r')
+        package_info = json.load(package_info_file)
+        package_info_file.close()
 
-    raven_client.context.merge({'tags': package_info})
+        raven_client.context.merge({'tags': package_info})
+    except:
+        # not deployed, probably a test
+        pass
+
     #raven_client.context.merge({'extra': {
     #    'event': event,
     #    'context': context
